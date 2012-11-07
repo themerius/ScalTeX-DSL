e8c7fc9fe1a1085397eb32e1b128d7bca32f4210
Sven Hodapp <sven.hodapp@gmail.com>
2012-11-07 11:39:52 +0100

    Scala DSL: Dynamic works now without explicit type cast

--- Scala/src/main/scala/main.scala
+++ Scala/src/main/scala/main.scala
@@ -3,15 +3,50 @@ package scaltex.main
 import scala.language.postfixOps
 import language.dynamics
 import scala.collection.mutable.HashMap
+import scala.reflect.ClassTag
 
 import scaltex.api._
 import scaltex.template.FraunhoferArticle
 
-object Main extends Dynamic {
+trait DynamicBase extends Dynamic {
+  def as[T:ClassTag]: T
+  def selectDynamic[T](name: String): DynamicBase
+  def updateDynamic(name:String)(value: Any)
+}
 
-  private val map = new HashMap[String, Any]
-  def selectDynamic(name: String): Any = {return map(name)}
-  def updateDynamic(name:String)(value: Any) = {map(name) = value}
+class ReflectionDynamic( val self: Any ) extends DynamicBase with Proxy {
+  def as[T:ClassTag]: T = { implicitly[ClassTag[T]].runtimeClass.asInstanceOf[Class[T]].cast( self ) }
+   // TODO: cache method lookup for faster access + handle NoSuchMethodError
+  def selectDynamic[T](name: String): DynamicBase = {
+    val ref = self.asInstanceOf[AnyRef]
+    val clazz = ref.getClass
+    clazz.getMethod(name).invoke( ref ) match {
+      case dyn: DynamicBase => dyn
+      case res => new ReflectionDynamic( res )
+    }
+  }
+  def updateDynamic( name: String )( value: Any ) = {
+    val ref = self.asInstanceOf[AnyRef]
+    val clazz = ref.getClass
+    // FIXME: check parameter type, and handle overloads
+    clazz.getMethods.find(_.getName == name+"_=").foreach{ meth =>
+      meth.invoke( ref, value.asInstanceOf[AnyRef] )
+    }
+  }
+}
+
+object Main extends DynamicBase {
+
+  def as[T:ClassTag]: T = { implicitly[ClassTag[T]].runtimeClass.asInstanceOf[Class[T]].cast( this ) }
+  private val map = new HashMap[String, DynamicBase]
+  def selectDynamic[T](name: String): DynamicBase = { map(name) }
+  def updateDynamic(name:String)(value: Any) = {
+    val dyn = value match {
+      case dyn: DynamicBase => dyn
+      case _ => new ReflectionDynamic( value )
+    }
+    map(name) = dyn
+  }
 
   § > "Überschrift"
 
@@ -25,7 +60,7 @@ object Main extends Dynamic {
 
   val text_1 =
   ++ txtref (() => s"""
-    Lorem ipsum Abb. ${Main.figname.asInstanceOf[scaltex.logic.Figure].number} dolor sit amet, consetetur sadipscing elitr, sed diam
+    Lorem ipsum Abb. ${Main.figname.number} dolor sit amet, consetetur sadipscing elitr, sed diam
     nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
     sed diam voluptua.
   """)
