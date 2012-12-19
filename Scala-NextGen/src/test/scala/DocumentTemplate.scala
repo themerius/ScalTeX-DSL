6cf5d1e8e88f93ddb83e54a1f19c0e158eb532c7
Sven Hodapp <sven.hodapp@gmail.com>
2012-12-19 18:04:39 +0100

    pending specs DocumentTemplate extended

--- Scala-NextGen/src/test/scala/DocumentTemplate.scala
+++ Scala-NextGen/src/test/scala/DocumentTemplate.scala
@@ -25,6 +25,24 @@ class DocumentTemplateSpec extends FunSpec with BeforeAndAfterEach {
 
   }
 
+  describe("A Areal") {
+
+    it("must have a companion object with the list of related entities") (pending)
+
+    it("is related to an append point, where pages are appended to") (pending)
+
+    it("has as minimum one assigned default page") (pending)
+
+    it("extends ordinary scala objects") (pending)
+
+    it("should own a implicit reference onto itslef") (pending)
+
+    it("should accept a implicit reference onto a Builder") (pending)
+
+    it("should have methods for manipulating the page numbering") (pending)
+
+  }
+
   describe("A Entity") {
 
     it("should be a half abstract type to make concrete") (pending)
@@ -33,11 +51,27 @@ class DocumentTemplateSpec extends FunSpec with BeforeAndAfterEach {
 
     it("should generate a JSON suitable for the template snippet") (pending)
 
+    it("knows an append point where it will appended to a page") (pending)
+
+    it("should be optional nameable") (pending)
+
+    it("should set it's name as reference to it's assigned object") (pending)
+
+    it("should have a bind method, for binding e.g. to ++") (pending)
+
     it("should be extensible with logic classes") (pending)
 
   }
 
-  describe("The binding ++") {
+  describe("A Page") {
+
+    it("is a special form of an entity, which groups entities together") (pending)
+
+    it("defines several append points, where entities are hooked in") (pending)
+
+  }
+
+  describe("The Entity binding ++") {
 
     it("implements the document-class's methods and binds it to corrosponding entity") (pending)
 
@@ -46,6 +80,14 @@ class DocumentTemplateSpec extends FunSpec with BeforeAndAfterEach {
     it("should return the entity") (pending)
 
     it("should be able to register the entities reference as property to the corrosponding areal") (pending)
+
+  }
+
+  describe("The ControlCommand binding !!") {
+
+    it("is able to change the page layout") (pending)
+
+    it("is able to manipulate configuration") (pending)
 
   }
 
